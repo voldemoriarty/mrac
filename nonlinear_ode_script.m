d222b6577310b011b410057c4f997c86bde3eadf
voldemoriarty <mamin.bee16seecs@seecs.edu.pk>
2019-12-27 16:02:59 +0500

    update gitignore to ignore .m backups

---
+++ nonlinear_ode_script.m
@@ -0,0 +1,29 @@
+m = 0.12; % the mass of the magnet in kg
+a = 1.65; % the a coefficient
+b = 6.20; % the b coefficient
+g = 9.81; % the gravity acceleration
+
+% define the timespan of the simulation
+time = [0 10];
+
+% define the step time
+step_time = 0;
+
+% define the initial condition
+y0 = [0 0];
+
+% define the amplitude of input
+amp = 3 * 10^4;
+
+% define the ode45 solver
+[to,yo] = ode45(@(t,y) next_state(t,y,step_time,m,a,b,g,amp), time, y0);
+
+% plot the results
+plot (to, yo(:,1));
+
+function dydt = next_state (t,y,step_time,m,a,b,g,amp)
+    dydt = zeros(2,1);
+    dydt(1) = y(2);
+    u = double(t >= step_time) .* amp;
+    dydt(2) = 1/m * ( (u)/(a*((y(1)+b)^4)) - m*g);
+end
